b7344dc516edafc9650469e0d217ddacfbc7e834
Wellington <wellingtongmb@gmail.com>
2024-11-16 08:32:55 -0300

    split service/logic from http method

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
-import { hash } from "bcryptjs"
 import { FastifyRequest, FastifyReply } from "fastify"
 
 import { prisma } from "@/lib/prisma"
+import { registerService } from "@/services/register"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -13,25 +13,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, 6)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email,
-        }
-    })
-
-    if (userWithSameEmail) {
-        return reply.status(409).send({ message: "User already created" })
+    try {
+        await registerService({
+            name, email, password
+        })
+    } catch (error) {
+        return reply.status(409).send()
     }
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash
-        }
-    })
 
     return reply.status(200).send({ message: "User Created" })
 }
