2aa39c4e1ecee4f90878fcf29f3c68cfef63ec79
Wellington <wellingtongmb@gmail.com>
2024-11-16 08:24:57 -0300

    implement password hash with bcryptjs and verify if user already created

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -13,7 +13,17 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, '')
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        }
+    })
+
+    if (userWithSameEmail) {
+        return reply.status(409).send({ message: "User already created" })
+    }
 
     await prisma.user.create({
         data: {
